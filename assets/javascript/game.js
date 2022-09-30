39840eae2192aeda8d328a298fcae770a3b5c572
Vinícius Silveira <vinifs19@gmail.com>
2022-09-30 00:55:43 -0300

    Added function to insert 'jump' class in pikachu element on keydown

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,13 @@ const pokeball = document.getElementById('pokeball');
 const clouds = docoment.getElementById('clouds');
 const bush = docoment.getElementById('bush');
 const gameOver = docoment.getElementById('game-over-text');
+
+const jump = () => {
+    pikachu.classList.add('jump');
+
+    setTimeout(() => {
+        pikachu.classList.remove('jump');
+    }, 500);
+}
+
+document.addEventListener('keydown', jump);
