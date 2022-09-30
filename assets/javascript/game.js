a0cc27145e7774e478b231d4899f3b6775280cb0
Vinícius Silveira <vinifs19@gmail.com>
2022-09-30 00:59:52 -0300

    Fixed problem in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 const pikachu = document.getElementById('pikachu');
 const pokeball = document.getElementById('pokeball');
-const clouds = docoment.getElementById('clouds');
-const bush = docoment.getElementById('bush');
-const gameOver = docoment.getElementById('game-over-text');
+const clouds = document.getElementById('clouds');
+const bush = document.getElementById('bush');
+const gameOver = document.getElementById('game-over-text');
 
 const jump = () => {
     pikachu.classList.add('jump');
