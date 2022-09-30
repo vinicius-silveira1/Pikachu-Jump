a2c41b2340c0687aefa0ebda7bbac66917bb2f80
Vinícius Silveira <vinifs19@gmail.com>
2022-09-30 00:52:37 -0300

    Added JavaScript id selectors for HTML elements

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+const pikachu = document.getElementById('pikachu');
+const pokeball = document.getElementById('pokeball');
+const clouds = docoment.getElementById('clouds');
+const bush = docoment.getElementById('bush');
+const gameOver = docoment.getElementById('game-over-text');
