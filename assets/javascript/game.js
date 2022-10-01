69dc0d300827b343d21da221e285aa7c47f78ba5
Vinícius Silveira <vinifs19@gmail.com>
2022-10-01 16:17:25 -0300

    Added losing condition and game loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,4 +12,34 @@ const jump = () => {
     }, 500);
 }
 
+// grab elements positions
+
+const gameLoop = setInterval(() => {
+    const pokeballPosition = pokeball.offsetLeft;
+    const pikachuPosition = window.getComputerStyle(pikachu).bottom.replace('px', '');
+    const cloudsPosition = clouds.offsetLeft;
+    const bushPosition = bush.offsetLeft;
+
+    if (pokeballPosition <= 115 && pikachuPosition <= 75 && pokeballPosition > 10) {
+        pokeball.style.animation = 'none';
+        pokeball.style.left = `${pokeballPosition}px`;
+
+        pikachu.style.animation = 'none';
+        pikachu.style.bottom = `${pikachuPosition}px`;
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
+
+        bush.style.animation = 'none';
+        bush.style.left = `${bushPosition}px`;
+
+        pikachu.src = "ed0cc7c0a1dcb68.png";
+        pikachu.style.width = '200px';
+        pikachu.style.marginBottom = '0';
+
+        gameOver.style.display = 'block';
+    }
+}, 10);
+
+
 document.addEventListener('keydown', jump);
